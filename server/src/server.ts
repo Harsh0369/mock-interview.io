3ed3df95decb6b1a1c1ccb0b3fb70dc99522b562
Harsh <harshagarwal81528@gmail.com>
2026-03-10 23:27:02 +0530

    feat(client): add @clerk/react dependency and integrate ClerkProvider in main.jsx; remove unused App.css feat(server): refactor server startup to include MongoDB connection; update package.json and package-lock.json with new dependencies

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import { connectDB } from "./config/db.js"
 
 dotenv.config()
 
@@ -7,6 +8,17 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+const startServer = async () => {
+    try {
+        await connectDB()
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    }
+    catch (err)
+    {
+        console.error("Error starting the server:", err);
+        process.exit(1);
+    }
+}
+startServer()
